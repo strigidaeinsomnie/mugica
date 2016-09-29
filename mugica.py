bde3cfb3bf60fccfccbcf6bffd0fb71aa1f51ec8
strigidaeinsomnie <strigidaeinsomnie@gmail.com>
2016-09-29 23:04:24 +0900

    h

---
+++ mugica.py
@@ -0,0 +1,28 @@
+import dropbox
+
+app_key = 'INSERT_APP_KEY'
+app_secret = 'INSERT_APP_SECRET'
+
+flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+
+authorize_url = flow.start()
+print '1. Go to: ' + authorize_url
+print '2. Click "Allow" (you might have to log in first)'
+print '3. Copy the authorization code.'
+code = input("Enter the authorization code here: ").strip()
+
+access_token, user_id = flow.finish(code)
+
+client = dropbox.client.DropboxClient(access_token)
+
+folder_metadata = client.metadata('/mugica')
+
+remotefiles = []
+for content in folder_metadata['contents']  :
+    remotefiles.append(content['path'])
+
+for tarfile in remotefiles :
+    f, metadata = client.get_file_and_metadata('[0]'.format(tarfile))
+    out = open('tarfile', 'wb')
+    out.write(f.read())
+    out.close()
