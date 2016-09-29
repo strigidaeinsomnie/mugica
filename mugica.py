30f2174596e910fe3da069b869a915deef5f2ed1
strigidaeinsomnie <strigidaeinsomnie@gmail.com>
2016-09-29 23:07:25 +0900

    h

--- mugica.py
+++ mugica.py
@@ -1,14 +1,14 @@
 import dropbox
 
-app_key = 'INSERT_APP_KEY'
-app_secret = 'INSERT_APP_SECRET'
+app_key = 'a7a2uzsk54m2rvw'
+app_secret = 'hx2zhh7gweo1qlv'
 
 flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
 
 authorize_url = flow.start()
-print '1. Go to: ' + authorize_url
-print '2. Click "Allow" (you might have to log in first)'
-print '3. Copy the authorization code.'
+print('1. Go to: ' + authorize_url)
+print('2. Click "Allow" (you might have to log in first)')
+print('3. Copy the authorization code.')
 code = input("Enter the authorization code here: ").strip()
 
 access_token, user_id = flow.finish(code)
