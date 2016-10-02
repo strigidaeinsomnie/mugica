4e9202ed14194fc1c4683c8ace0aeb9051a0598a
strigidaeinsomnie <strigidaeinsomniegmail.com>
2016-10-02 18:10:32 +0900

    h

--- mugica.py
+++ mugica.py
@@ -67,11 +67,12 @@ def lake_play(tardir_lake) :
 
 #-----------------------------------
 
-web = os.system('ping -c 5 google.com')
-
-if web is 0 :
-    drop_authorize(access_token)
-    drop_is_lake(tardir_drop, tardir_lake)
 
 while True :
+    web = os.system('ping -c 5 google.com')
+
+    if web is 0 :
+        drop_authorize(access_token)
+        drop_is_lake(tardir_drop, tardir_lake)
+
     lake_play(tardir_lake)
